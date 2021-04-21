051f0c91679b5e603140893764f31b86d2d47435
Dawid Kołakowski <dawidkzk@gmail.com>
2021-04-21 22:46:39 +0200

    added comments

--- js_repos_visual.py
+++ js_repos_visual.py
@@ -3,15 +3,13 @@ import requests
 from plotly.graph_objs import Bar, Layout
 from plotly import offline
 
-
-
 # Make an API call and store the response
 url = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
-
+# Process response
 response_dict = r.json()
 repo_dicts = response_dict['items']
 repo_links, stars, labels, forks = [], [], [], []
@@ -28,7 +26,7 @@ for repo_dict in repo_dicts:
     label = f"{owner}<br/> {description}"
     labels.append(label)
 
-
+# Make a visualization
 data = [{
     'type': 'bar',
     'x': repo_links,
@@ -40,6 +38,7 @@ data = [{
     'opacity': 0.6,
     }
     }]
+
 my_layout = {
     'title': 'Most JavaScript starred repositories on GitHub',
     'titlefont': {'size': 28},
